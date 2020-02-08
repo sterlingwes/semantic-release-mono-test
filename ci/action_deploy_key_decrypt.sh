95eba06ff94183ddf1b3db27add35c9be8e71d1e
Wes Johnson <wjohnson@wealthsimple.com>
2020-02-08 13:37:00 -0500

    ci(toolchain): try again

--- ci/action_deploy_key_decrypt.sh
+++ ci/action_deploy_key_decrypt.sh
@@ -7,8 +7,8 @@ chmod 600 $HOME/.ssh/*
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$DEPLOY_KEY_PASS" \
 --output $HOME/.ssh/id_rsa action_deploy_key.gpg
-ls
-ls -la $HOME/
-ls -la $HOME/.ssh
+echo $(ls)
+echo $(ls -la $HOME/)
+echo $(ls -la $HOME/.ssh)
 
 chmod 0644 $HOME/.ssh/id_rsa
