ee3d399e53817dd368b4625966e58adf39a3bcce
Wes Johnson <wjohnson@wealthsimple.com>
2020-02-08 13:39:52 -0500

    fix(toolchain): rm other chmod command and fix key perm

--- ci/action_deploy_key_decrypt.sh
+++ ci/action_deploy_key_decrypt.sh
@@ -2,13 +2,9 @@
 
 mkdir -p $HOME/.ssh
 chmod 700 $HOME/.ssh
-chmod 600 $HOME/.ssh/*
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$DEPLOY_KEY_PASS" \
 --output $HOME/.ssh/id_rsa action_deploy_key.gpg
-echo $(ls)
-echo $(ls -la $HOME/)
-echo $(ls -la $HOME/.ssh)
 
-chmod 0644 $HOME/.ssh/id_rsa
+chmod 600 $HOME/.ssh/id_rsa
