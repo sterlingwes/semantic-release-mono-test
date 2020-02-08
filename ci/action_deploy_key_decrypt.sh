ab809e16aa6cd2d708c7200bbb4ac5a930a8acdb
Wes Johnson <wjohnson@wealthsimple.com>
2020-02-08 13:25:01 -0500

    ci(toolchain): add deploy key for push access

---
+++ ci/action_deploy_key_decrypt.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p $HOME/.ssh
+chmod 700 $HOME/.ssh
+chmod 600 ~/.ssh/*
+
+# --batch to prevent interactive command --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$DEPLOY_KEY_PASS" \
+--output $HOME/.ssh/id_rsa action_deploy_key.gpg
