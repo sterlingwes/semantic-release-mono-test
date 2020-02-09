ef05259a8b50cb2c3a54b3dd5e93024fabe395b3
Wes Johnson <swj@wesquire.ca>
2020-02-09 12:25:06 -0500

    Allow for PR changelog preview (#7)
    * docs: add README for some-package
    
    * ci: add PR notes plugin to existing config
    
    * add prettier config
    
    * ci: update branch config to use extended config
    
    * chore: debug
    
    * ci: trigger releases on branch
    
    * ci: specify explicit branch name
    
    * ci: pull branch name from env var
    
    * ci: new branch name extractor
    
    * ci: try another env var
    
    * ci: thinks its on master for some reason
    
    * ci: does not need verify step
    
    * ci: workaround dumb array of steps
    
    * fix: patch package for pr to export
    
    * ci: ref plugin implementations
    
    * ci: handle array of plugin steps
    
    * ci: try to augment results by adding to chain
    
    * ci: cheat some more
    
    * fix: patch package properly w/ decorated plugin methods
    
    * chore: debug results
    
    * fix: oops
    
    * feat(some-package): log something
    
    * chore: debug plugin w/ logs
    
    * ci: ensure results passed to generateNotes & debug
    
    * fix: ensure analyze step returns type
    
    * chore: debug generateNotes step
    
    * fix(my-package): too many bangs
    
    * ci: ensure release notes are returned

--- ci/semantic_release_monorepo.js
+++ ci/semantic_release_monorepo.js
@@ -1,60 +1,94 @@
-const semanticReleaseNpm = require('@semantic-release/npm')
-const semanticReleaseGit = require('@semantic-release/git')
-const semanticReleaseGithub = require('@semantic-release/github')
-const semanticReleaseChanelog = require('@semantic-release/changelog')
-const {analyzeCommits, generateNotes, tagFormat} = require('semantic-release-monorepo')
+const semanticReleaseNpm = require('@semantic-release/npm');
+const semanticReleaseGit = require('@semantic-release/git');
+const semanticReleaseGithub = require('@semantic-release/github');
+const semanticReleasePrNotes = require('semantic-release-github-pr');
+const semanticReleaseChanelog = require('@semantic-release/changelog');
+const { analyzeCommits, generateNotes, tagFormat } = require('semantic-release-monorepo');
 
-const noPublishNpmConfig = (pluginConfig) => ({
+const noPublishNpmConfig = pluginConfig => ({
   ...pluginConfig,
-  npmPublish: false
-})
+  npmPublish: false,
+});
 
-const configure = (context) => ({
+const configure = context => ({
   ...context,
   options: {
-    ...context.options || {},
+    ...(context.options || {}),
     publish: [
       {
         path: '@semantic-release/npm',
-        npmPublish: false
-      }
-    ]
-  }
-})
+        npmPublish: false,
+      },
+    ],
+  },
+});
+
+const branchBuild = !!process.env.SEMANTIC_RELEASE_PR;
 
 /**
  * default semantic-release plugins:
- * 
+ *
  * @semantic-release/commit-analyzer
  * @semantic-release/release-notes-generator
  * @semantic-release/npm
  * @semantic-release/github
  */
 
+console.log('branch build config:', branchBuild);
+
+const executeSteps = (asyncSteps, pluginConfig, context) =>
+  asyncSteps.reduce(
+    (chain, step) =>
+      chain.then(results => {
+        const result = step(pluginConfig, context);
+        return [...results, result];
+      }),
+    Promise.resolve([]),
+  );
+
 module.exports = {
-  analyzeCommits,
-  generateNotes,
+  analyzeCommits: async (pluginConfig, context) => {
+    const results = await executeSteps(analyzeCommits, pluginConfig, context);
+    const resolvedResults = await Promise.all(results);
+    if (branchBuild) {
+      await semanticReleasePrNotes.impl.analyzeCommits(pluginConfig, context, resolvedResults);
+    }
+
+    const [releaseType] = resolvedResults.filter(releaseType => !!releaseType);
+    return releaseType;
+  },
+
+  generateNotes: async (pluginConfig, context) => {
+    const results = await executeSteps(generateNotes, pluginConfig, context);
+    const resolvedResults = await Promise.all(results);
+    if (branchBuild) {
+      await semanticReleasePrNotes.impl.generateNotes(pluginConfig, context, resolvedResults);
+    }
+    const [notes] = resolvedResults.filter(notes => !!notes);
+    return notes;
+  },
+
   tagFormat,
 
   verifyConditions: async (pluginConfig, context) => {
     semanticReleaseGit.verifyConditions(pluginConfig, context);
     await semanticReleaseChanelog.verifyConditions(pluginConfig, context);
-    await semanticReleaseNpm.verifyConditions(noPublishNpmConfig(pluginConfig), configure(context))
+    await semanticReleaseNpm.verifyConditions(noPublishNpmConfig(pluginConfig), configure(context));
     await semanticReleaseGithub.verifyConditions(pluginConfig, context);
   },
 
   prepare: async (pluginConfig, context) => {
     await semanticReleaseChanelog.prepare(pluginConfig, context);
-    await semanticReleaseNpm.prepare(noPublishNpmConfig(pluginConfig), configure(context))
+    await semanticReleaseNpm.prepare(noPublishNpmConfig(pluginConfig), configure(context));
     await semanticReleaseGit.prepare(pluginConfig, context);
   },
 
   publish: async (pluginConfig, context) => {
-    await semanticReleaseNpm.publish(noPublishNpmConfig(pluginConfig), configure(context))
+    await semanticReleaseNpm.publish(noPublishNpmConfig(pluginConfig), configure(context));
     await semanticReleaseGithub.publish(pluginConfig, context);
   },
 
   addChannel: async (pluginConfig, context) => {
-    await semanticReleaseNpm.addChannel(noPublishNpmConfig(pluginConfig), configure(context))
+    await semanticReleaseNpm.addChannel(noPublishNpmConfig(pluginConfig), configure(context));
   },
-}
+};
